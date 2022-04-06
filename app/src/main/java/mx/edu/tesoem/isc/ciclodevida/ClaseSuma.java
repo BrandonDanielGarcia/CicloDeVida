f563436ceef6f13fba8b8a793c6d0f8eeb5dd69b
BrandonDanielGarcia <brandon.dani123@gmail.com>
2022-04-06 10:13:12 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/ciclodevida/ClaseSuma.java
@@ -0,0 +1,7 @@
+package mx.edu.tesoem.isc.ciclodevida;
+
+public class ClaseSuma {
+    public static final int incrementar(int numero){
+        return (numero+1);
+    }
+}
---
+++ app/src/main/java/mx/edu/tesoem/isc/ciclodevida/Modelo/ModeloSuma.java
@@ -0,0 +1,15 @@
+package mx.edu.tesoem.isc.ciclodevida.Modelo;
+
+import androidx.lifecycle.ViewModel;
+
+public class ModeloSuma extends ViewModel {
+    int numero;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
